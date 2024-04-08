c9a80bbf8f1d5d4781a6c1ec0d514bda1cf3115e
PatitoDestructor <cristianvelez308@gmail.com>
2024-04-08 01:30:11 -0500

    gola

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 import express from 'express';
-import prendaRouter from './routes/prenda.js';
+import productosRouter from './routes/prenda.js';
 import './database/config.js'
 import cors from 'cors'
-
 const port = 3000;
 const app = express();
 app.use(express.json());
-app.use('/api/v1', prendaRouter)
-app.use(cors())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+
+
+app.use(cors());
+app.use('/api/v1', productosRouter)
 app.get('/', (req, res) => {
     res.redirect('/api/v1/');
 })
 app.listen(port, () => {
-    console.log('API RUNNING ON PORT 3000 🚀 YOU CAN SEE THE ROUTES IN localhost:3000/api/v1/prenda')
+    console.log('API RUNNING ON PORT 3000 🚀 YOU CAN SEE THE ROUTES IN localhost:3000/api/v1/productos')
 })
