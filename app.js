d8f4243b701082c70990183ebda712189643e698
PatitoDestructor <cristianvelez308@gmail.com>
2024-04-08 01:23:11 -0500

    ddd

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(express.json());
 app.use('/api/v1', prendaRouter)
 app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.get('/', (req, res) => {
     res.redirect('/api/v1/');
 })
